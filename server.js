56e6b0916073e69324130d82f7f368ff6f384191
Coymw1 <coymw1@ad.unc.edu>
2023-03-17 18:23:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ fs.readFile('./public/index.html', (err, data) => {
     if (err) {
         return console.error(err);
     }
-    console.log(data);
+    return data;
 });
 
 
@@ -40,7 +40,7 @@ fs.readFile('./public/index.html', (err, data) => {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
+    res.end(fs.readFile());
 
 });
 
